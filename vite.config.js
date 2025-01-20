51b5f0e453fbee33c9f50a9729893afeee61c5ca
Ayush-Malhotra <aayushmalhotra003@gmail.com>
2025-01-20 21:59:16 +0530

    Axios Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['axios'], // Pre-bundle axios for development
+  },
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
   server : {
     proxy : {
       '/api' : "http://localhost:3000"
