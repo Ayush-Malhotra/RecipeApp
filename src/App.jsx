6837a586b3e0979e602ee146a3be7f16e373cdf8
Ayush-Malhotra <aayushmalhotra003@gmail.com>
2025-01-20 22:12:38 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect , useRef} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import axios from 'axios';
+
 import RecipeCard from './RecipeCard';
 import RecipeDetail from './RecipeDetail';
 import Header from './Header';
